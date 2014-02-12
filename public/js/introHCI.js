b04d8233903d2e3e6858177c724ebb5732ce7e3e
Tomas Forsyth Rosin <tomasrosin@gmail.com>
2014-02-12 21:52:34 +0000

    removed annoying console message

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -47,7 +47,6 @@ function randomizeColors(e) {
 
 function setColors(colorData) {
 	var colors = colorData.colors.hex;
-	console.log(colors);
 	$('body').css('background-color', colors[0]);
 	$('.thumbnail').css('background-color', colors[1]);
 	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
